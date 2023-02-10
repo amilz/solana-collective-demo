590081d6056c4510e06296baa9826b53722632a5
amilz.sol <85324096+amilz@users.noreply.github.com>
2023-02-10 10:59:45 -0800

    update website

--- 03-web/src/components/RequestAirdrop.tsx
+++ 03-web/src/components/RequestAirdrop.tsx
@@ -1,57 +1,61 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
+import { LAMPORTS_PER_SOL, PublicKey, TransactionSignature } from '@solana/web3.js';
 import { FC, useCallback } from 'react';
 import { notify } from "../utils/notifications";
 import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
+import { Metaplex, walletAdapterIdentity } from '@metaplex-foundation/js';
 
 export const RequestAirdrop: FC = () => {
     const { connection } = useConnection();
     const { publicKey } = useWallet();
+    const walletAdapter = useWallet();
     const { getUserSOLBalance } = useUserSOLBalanceStore();
 
-    const onClick = useCallback(async () => {
+    const onClick = (async () => {
         if (!publicKey) {
             console.log('error', 'Wallet not connected!');
             notify({ type: 'error', message: 'error', description: 'Wallet not connected!' });
             return;
         }
 
-        let signature: TransactionSignature = '';
+        let METAPLEX = Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter));
+        let candyMachine = await METAPLEX.candyMachines().findByAddress({ address: new PublicKey('EMPapSGD9uPkLoa86wjwtWuawAsoNr3bbyCFT7W5V1Mi') })
+
 
         try {
-            signature = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
+            const txBuilder = await METAPLEX.candyMachines().builders().mint({
+                candyMachine,
+                collectionUpdateAuthority: new PublicKey('scNkbXvJQ3L83JmvYfVmuLaq9UidaiuYzQaHD41KXt5')
+            })
+            const blockhash = await METAPLEX.rpc().getLatestBlockhash();
 
-            // Get the lates block hash to use on our transaction and confirmation
-            let latestBlockhash = await connection.getLatestBlockhash()
-            await connection.confirmTransaction({ signature, ...latestBlockhash }, 'confirmed');
-
-            notify({ type: 'success', message: 'Airdrop successful!', txid: signature });
-
-            getUserSOLBalance(publicKey, connection);
+            let tx = txBuilder.toTransaction(blockhash);
+            let { signature, confirmResponse } = await METAPLEX.rpc().sendAndConfirmTransaction(txBuilder, { commitment: 'finalized' });
+            notify({ type: 'success', message: 'Mint successful!', txid: signature });
         } catch (error: any) {
-            notify({ type: 'error', message: `Airdrop failed!`, description: error?.message, txid: signature });
-            console.log('error', `Airdrop failed! ${error?.message}`, signature);
+            console.log(error); 
         }
-    }, [publicKey, connection, getUserSOLBalance]);
+    });
+
 
     return (
 
         <div className="flex flex-row justify-center">
-                <div className="relative group items-center">
-                    <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
+            <div className="relative group items-center">
+                <div className="m-1 absolute -inset-0.5 bg-gradient-to-r from-indigo-500 to-fuchsia-500 
                     rounded-lg blur opacity-20 group-hover:opacity-100 transition duration-1000 group-hover:duration-200 animate-tilt"></div>
-            
-                        <button
-                            className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
-                            onClick={onClick}
-                            >
-                                <span>Airdrop 1 </span>
-                
-                        </button>
-                </div>
+
+                <button
+                    className="px-8 m-2 btn animate-pulse bg-gradient-to-br from-indigo-500 to-fuchsia-500 hover:from-white hover:to-purple-300 text-black"
+                    onClick={onClick}
+                >
+                    <span>Mint</span>
+
+                </button>
+            </div>
         </div>
 
-        
+
     );
 };
 
